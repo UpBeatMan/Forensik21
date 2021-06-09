b97d94b659e8e929b805aec954bb7d38fd86108a
Jan Weil <albcycle@gmx.net>
2021-06-09 11:11:32 +0200

    Clean Code (PEP8) & old file structure archive

--- threadingPOC/oldFileStructure/database/tables/formhistory.py
+++ threadingPOC/oldFileStructure/database/tables/formhistory.py
@@ -5,20 +5,18 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 __version__ = '0.1.0'
 
-# global variables
+# browser time format
 DT_MILLI_ZEROED_MICRO = "datetime_milliseconds_zeroed_microseconds"
-IDENTIFIER = ""
-
 # objects
 Base = declarative_base()
 
 
 class FormHistory(Base):
     ID = "ID"
-    FIELDNAME = "Feldname"
+    FIELD_NAME = "Feldname"
     VALUE = "Eingabewert"
-    FIRSTUSED = "Zum ersten mal verwendet"
-    LASTUSED = "Zuletzt genutzt"
+    FIRST_USED = "Zum ersten mal verwendet"
+    LAST_USED = "Zuletzt genutzt"
 
     __tablename__ = 'moz_formhistory'  # HISTORY
 
@@ -28,25 +26,19 @@ class FormHistory(Base):
     first_used_timestamp = Column("firstUsed", Integer)
     last_used_timestamp = Column("lastUsed", Integer)
 
-    IDENTIFIER = id
-
     # @orm.reconstructor - throws error, not knowing orm name
     def __init__(self):
         self.attr_list = []
-        self.attr_list.append(BaseAttribute(FIELDNAME, OTHER, self.field_name))
+        self.attr_list.append(BaseAttribute(FIELD_NAME, OTHER, self.field_name))
         self.attr_list.append(BaseAttribute(VALUE, OTHER, self.value))
-        self.attr_list.append(
-            BaseAttribute(FIRSTUSED, DT_MILLI_ZEROED_MICRO, self.first_used_timestamp)
-        )
-        self.attr_list.append(
-            BaseAttribute(LASTUSED, DT_MILLI_ZEROED_MICRO, self.last_used_timestamp)
-        )
+        self.attr_list.append(BaseAttribute(FIRST_USED, DT_MILLI_ZEROED_MICRO, self.first_used_timestamp))
+        self.attr_list.append(BaseAttribute(LAST_USED, DT_MILLI_ZEROED_MICRO, self.last_used_timestamp))
 
     def __repr__(self):
         return "<FormHistory(field_name='%s', value='%s', first_used_timestamp='%d', last_used_timestamp='%d')>" % (
             self.field_name, self.value, self.first_used_timestamp, self.last_used_timestamp)
 
-    def get_count(self, q):
-        count_q = q.statement.with_only_columns([func.count()]).order_by(None)
-        count = q.session.execute(count_q).scalar()
-        return count
+    def count_rows(self, dbms):
+        count_q = dbms.statement.with_only_columns([func.count()]).order_by(None)
+        counted = dbms.session.execute(count_q).scalar()
+        return counted
